695516c8a3fc8477b877b8ac6322686b2b29e0f9
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-05 16:36:19 +0530

    varshini5.c
    program

---
+++ varshini5.c
@@ -0,0 +1,17 @@
+#include<stduo.h>
+main()
+int n;num=0;i=0;
+printf('enter the numbers\t");
+scanf(%d&n);
+for(i=2;i<n,i++);
+{
+if(n% i==0)
+num=num+1
+break;
+}
+if(num==1)
+printf("it is not a prime num b");
+else
+printf("it is a prime num");
+getch();
+}
