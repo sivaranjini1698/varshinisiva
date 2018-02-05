3756dfc9c40fe64d8cecf3f4ce3176539cd5d86a
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-05 15:53:01 +0530

    vzrshini.c
    program

---
+++ varshini2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+struct student
+{
+int id;
+char name[20];
+float percentage;
+}
+int main();
+{
+struct student record=0;
+record.id=1;
+strcpy percentage=86.5
+printf("is%d\n"record.id)
+printf("name is%s\n",record.name)
+printf("percentge is%f\n",record percentage;
+return 0;
+}
