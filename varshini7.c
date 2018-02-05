615034fb45df43c62ccc0d926b5370ae6f4727bc
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-05 16:51:42 +0530

    varshini7.c
    progrm

---
+++ varshini7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+voidmain()
+{
+inta=saturday;
+intb=monday;
+printf("enter the saturday");
+scanf("%c",& saturday");
+printf("enter the monday");
+scanf("%c",& monday");
+}
+else
+{
+printf("value as monday");
+getch0
+}
