332d5fc07553dbb75aa1c319482f5b8b4575a0f5
sivaranjini1698 <31438762+sivaranjini1698@users.noreply.github.com>
2018-02-05 16:23:08 +0530

    varshini4.c
    program

---
+++ varshini4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+intmain()
+{
+int a=aab;
+int b=xxy;
+printf("enter tha value a=aab");
+scanf("%s",& a);
+printf("enter the value b=xxb");
+scanf("%d",& b);
+}
+else
+{
+print f("%d is changed the value")
+getch();
+}
